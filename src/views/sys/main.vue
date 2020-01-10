2d180c1088f9f9337ce311ef6e746197c9296d60
huang1071425505 <1665581086@qq.com>
2020-01-10 16:53:46 +0800

    用户登录

--- src/views/sys/main.vue
+++ src/views/sys/main.vue
@@ -3,21 +3,40 @@
     <h1>111111111111111</h1>
     <el-button type="warning" @click="ceshi">测试</el-button>
     <div v-if="userName!=''">{{userName}} is pig</div>
+    <el-button type="warning" @click="login">测试登录</el-button>
+    
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import qs from 'qs'
 export default {
     data(){
         return{
-            userName:""
+            userName:"",
+            token:""
         }
     },
     methods:{
         ceshi(){
-            axios.get("/api/SysUsers/myInfo").then(res=>{
+            axios.get("/api/sysUsers/myInfo",
+            {
+            headers: {
+                token:this.token
+            },
+        }).then(res=>{
                 this.userName=res.data.data[0].userName
+            })
+        },
+        login(){
+            let postData = qs.stringify({
+                    userCode:"123",
+                password:"123456"
+            })
+            axios.post("/api/sysUsers/login",
+            postData).then(res=>{
+                this.token=res.data.msg;
             })
         }
     }
